06d42c9fac9b845714210e0bdc56afd3fe9177d0
sukiboo <dereventsov@gmail.com>
2023-10-03 08:11:14 -0400

    add shell variables

--- not_dropbox.sh
+++ not_dropbox.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# read config file
+echo $(date)
+source /Users/suki/scripts/config
+
 # pull from client
-sshpass -p $CLIENT_PASS rsync -cavu $CLIENT_IP:$CLIENT_DIR $SERVER_DIR
+$SSHPASS_PATH -p $CLIENT_PASS $RSYNC_PATH -cavu $CLIENT_IP:$CLIENT_DIR $SERVER_DIR
 
 # push to client
-sshpass -p $CLIENT_PASS rsync -cavu --delete $SERVER_DIR $CLIENT_IP:$CLIENT_DIR
+$SSHPASS_PATH -p $CLIENT_PASS $RSYNC_PATH -cavu --delete $SERVER_DIR $CLIENT_IP:$CLIENT_DIR
 
